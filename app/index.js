7df437ee8abd6eb4777f3bedc1910137b34247b1
Jeff DeWitt <dewittjr@hendrix.edu>
2018-02-05 00:52:58 -0800

    add index files

---
+++ app/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import * as reducers from 'redux/modules'
+import { MainContainer } from 'containers'
+
+const store = createStore(combineReducers(reducers), compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : (f) => f
+))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <MainContainer />
+  </Provider>,
+  document.getElementById('app')
+)
